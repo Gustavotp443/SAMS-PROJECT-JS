6e30273dc7fe9103e66836f286e3e0e56af47d75
Gustavotp443 <gustavotp443@gmail.com>
2024-02-24 14:58:05 -0300

    created-app

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,7 +3,7 @@ import {server} from "./server/server";
 
 //Rodar npm ts-node-dev ./src/index.ts
 server.listen(3333, ()=>{
-    console.log("App rodando!")
-    console.log("Porta: 3333")
-    console.log("http://localhost:3333")
+	console.log("App rodando!");
+	console.log("Porta: 3333");
+	console.log("http://localhost:3333");
 });
--- app/src/server/server.ts
+++ app/src/server/server.ts
@@ -3,8 +3,8 @@ import express from "express";
 const server = express();
 
 server.get("/", (req,res) => {
-    return res.send("Olá bem vindo ao SAMS")
-})
+	return res.send("Olá bem vindo ao SAMS");
+});
 
 
 export { server };
