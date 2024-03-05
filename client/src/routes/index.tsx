fbcbc26ab8eaf083d0acf8fff7554fa00af86f60
gustavo <gustavo.padrini@cportal.com.br>
2024-03-05 10:51:18 -0300

    toggle theme

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,11 +1,22 @@
+import React from "react";
 import { Button } from "@mui/material";
-import { Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
-    return(
-        <Routes>
-            <Route path="/home" element={<Button variant="contained" color="primary">TESTE</Button>}/>
-            <Route path="*" element={<Navigate to="/home"/>}/>
-        </Routes>
-    );
+  const { toggleTheme } = useAppThemeContext();
+
+  return (
+    <Routes>
+      <Route
+        path="/home"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            TOGGLE THEME
+          </Button>
+        }
+      />
+      <Route path="*" element={<Navigate to="/home" />} />
+    </Routes>
+  );
 };
