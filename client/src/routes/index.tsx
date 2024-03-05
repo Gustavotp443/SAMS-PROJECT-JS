34941276d102b0395fecdc6382fa79de68f59f9c
gustavo <gustavo.padrini@cportal.com.br>
2024-03-05 10:45:06 -0300

    Theme Context Setted

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import { Button } from "@mui/material";
+import { Routes, Route, Navigate} from "react-router-dom";
+
+export const AppRoutes = () => {
+    return(
+        <Routes>
+            <Route path="/home" element={<Button variant="contained" color="primary">TESTE</Button>}/>
+            <Route path="*" element={<Navigate to="/home"/>}/>
+        </Routes>
+    );
+};
