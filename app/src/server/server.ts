84e14a1d22466f517f16c612f2302a6f39699490
Gustavotp443 <gustavotp443@gmail.com>
2024-03-21 21:06:52 -0300

    server-configs-cors-error

--- app/src/server/server.ts
+++ app/src/server/server.ts
@@ -6,12 +6,7 @@ import { router } from "./routes";
 //CONFIGURAÇÕES DO SERVIDOR
 const server = express();
 
-const originProd =
-  process.env.NODE_ENV === "production"
-    ? "https://sams-project-js.vercel.app"
-    : "http://localhost:3000";
-
-server.use(cors({ origin: originProd }));
+server.use(cors());
 
 server.use(express.json());
 server.use(router);
