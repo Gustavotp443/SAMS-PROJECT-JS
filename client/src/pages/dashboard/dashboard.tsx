2eb8312ecf6bb3a5aa367f7b2b8e84cf1d7a3a53
Gustavotp443 <gustavotp443@gmail.com>
2024-06-25 18:07:09 -0300

    design changes

--- client/src/pages/dashboard/dashboard.tsx
+++ client/src/pages/dashboard/dashboard.tsx
@@ -1,11 +1,26 @@
+import { Card, CardMedia, Paper, Typography } from "@mui/material";
 import { ListTolls } from "../../shared/components";
 import { LayoutBasePage } from "../../shared/layouts";
+import img from "./../../img/fundo2.png";
 
 export const Dashboard = () => {
   return (
-    <LayoutBasePage
-      titulo="SAMS"
-      barraDeFerramentas={<ListTolls showNewButton={false} />}
-    ></LayoutBasePage>
+    <LayoutBasePage titulo="SAMS">
+      <Paper sx={{ padding: 4 }}>
+        <Typography variant="h5" align="center">
+          Olá, seja bem-vindo ao SAMS!
+        </Typography>
+        <Card
+          sx={{
+            maxWidth: 400,
+            margin: "auto",
+            top: "20px",
+            position: "relative"
+          }}
+        >
+          <CardMedia component="img" height="auto" image={img} />
+        </Card>
+      </Paper>
+    </LayoutBasePage>
   );
 };
--- client/src/shared/components/login/register.tsx
+++ client/src/shared/components/login/register.tsx
@@ -15,6 +15,9 @@ import * as yup from "yup";
 import { userService } from "../../services/api/users/userService";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
+import bgImg1 from "./../../../img/fundo1.png";
+import bgImg2 from "./../../../img/fundo2.png";
+
 const registerSchema = yup.object().shape({
   name: yup.string().required().min(3).max(100),
   email: yup.string().required().email().max(100),
@@ -40,6 +43,24 @@ export const Register: React.FC<IRegisterProps> = ({
   const [nameError, setNameError] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+
+  interface BackgroundBoxProps {
+    image: string;
+    zIndex: number;
+  }
+  const BackgroundBox: React.FC<BackgroundBoxProps> = ({ image, zIndex }) => (
+    <Box
+      sx={{
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        zIndex: zIndex
+      }}
+    />
+  );
 
   const handleSubmit = () => {
     setIsLoading(true);
@@ -74,80 +95,90 @@ export const Register: React.FC<IRegisterProps> = ({
       alignItems="center"
       justifyContent="center"
     >
-      <Card>
-        <CardContent>
-          <Box display="flex" flexDirection="column" gap={2} width={250}>
-            <Typography variant="h6" align="center">
-              Cadastro
-            </Typography>
-            <TextField
-              fullWidth
-              type="name"
-              label="Nome"
-              value={name}
-              disabled={isLoading}
-              error={!!nameError}
-              helperText={nameError}
-              onChange={e => setName(e.target.value)}
-              onKeyDown={() => setNameError("")}
-            />
-            <TextField
-              fullWidth
-              type="email"
-              label="Email"
-              value={email}
-              disabled={isLoading}
-              error={!!emailError}
-              helperText={emailError}
-              onChange={e => setEmail(e.target.value)}
-              onKeyDown={() => setEmailError("")}
-            />
-            <TextField
-              fullWidth
-              type={showPassword ? "text" : "password"}
-              label="Senha"
-              value={password}
-              disabled={isLoading}
-              error={!!passwordError}
-              helperText={passwordError}
-              onChange={e => setPassword(e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() => setShowPassword(!showPassword)}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-              onKeyDown={() => setPasswordError("")}
-            />
-          </Box>
-        </CardContent>
-        <CardActions>
-          <Box width="100%" display="flex" justifyContent="center" gap={1}>
-            <Button
-              variant="contained"
-              onClick={handleSubmit}
-              disabled={isLoading}
-              endIcon={
-                isLoading ? (
-                  <CircularProgress
-                    variant="indeterminate"
-                    color="inherit"
-                    size={20}
-                  />
-                ) : undefined
-              }
-            >
-              Cadastrar
-            </Button>
-            <Box>
+      {/* Fundo mais profundo */}
+      <BackgroundBox image={bgImg2} zIndex={1} />
+
+      {/* Fundo mais próximo */}
+      <BackgroundBox image={bgImg1} zIndex={2} />
+      <Box>
+        <Box
+          sx={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: 1000,
+            textShadow: "1px 1px 0px #fff",
+            textAlign: "center",
+            color: "#cd9a02",
+            top: "20%"
+          }}
+        >
+          <Typography variant="h4">Olá! Você está no SAMS</Typography>
+        </Box>
+        <Card
+          sx={{
+            zIndex: 999,
+            boxShadow: "6px 6px 4px rgba(255, 193, 7, 1)", // Sombra amarela
+            position: "relative"
+          }}
+        >
+          <CardContent>
+            <Box display="flex" flexDirection="column" gap={2} width={250}>
+              <Typography variant="h6" align="center">
+                Cadastro
+              </Typography>
+              <TextField
+                fullWidth
+                type="name"
+                label="Nome"
+                value={name}
+                disabled={isLoading}
+                error={!!nameError}
+                helperText={nameError}
+                onChange={e => setName(e.target.value)}
+                onKeyDown={() => setNameError("")}
+              />
+              <TextField
+                fullWidth
+                type="email"
+                label="Email"
+                value={email}
+                disabled={isLoading}
+                error={!!emailError}
+                helperText={emailError}
+                onChange={e => setEmail(e.target.value)}
+                onKeyDown={() => setEmailError("")}
+              />
+              <TextField
+                fullWidth
+                type={showPassword ? "text" : "password"}
+                label="Senha"
+                value={password}
+                disabled={isLoading}
+                error={!!passwordError}
+                helperText={passwordError}
+                onChange={e => setPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setShowPassword(!showPassword)}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+                onKeyDown={() => setPasswordError("")}
+              />
+            </Box>
+          </CardContent>
+          <CardActions>
+            <Box width="100%" display="flex" justifyContent="center" gap={1}>
               <Button
                 variant="contained"
+                onClick={handleSubmit}
                 disabled={isLoading}
                 endIcon={
                   isLoading ? (
@@ -158,14 +189,31 @@ export const Register: React.FC<IRegisterProps> = ({
                     />
                   ) : undefined
                 }
-                onClick={() => setRegister(false)}
               >
-                Voltar ao Login
+                Cadastrar
               </Button>
+              <Box>
+                <Button
+                  variant="contained"
+                  disabled={isLoading}
+                  endIcon={
+                    isLoading ? (
+                      <CircularProgress
+                        variant="indeterminate"
+                        color="inherit"
+                        size={20}
+                      />
+                    ) : undefined
+                  }
+                  onClick={() => setRegister(false)}
+                >
+                  Voltar ao Login
+                </Button>
+              </Box>
             </Box>
-          </Box>
-        </CardActions>
-      </Card>
+          </CardActions>
+        </Card>
+      </Box>
     </Box>
   );
 };
