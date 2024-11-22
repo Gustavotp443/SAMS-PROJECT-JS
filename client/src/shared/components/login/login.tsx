cbaf91f9e3f35ee6501f8095d85ace8230f4c613
Gustavotp443 <gustavotp443@gmail.com>
2024-11-22 00:34:13 -0300

    atualização tela login

--- client/src/shared/components/login/login.tsx
+++ client/src/shared/components/login/login.tsx
@@ -110,113 +110,113 @@ export const Login: React.FC<ILoginProps> = ({ children }) => {
       display="flex"
       alignItems="center"
       justifyContent="center"
+      sx={{ position: "relative" }} // Adicionando 'relative' para controle de posicionamento dos elementos internos
     >
-      {/* Fundo mais profundo */}
       <BackgroundBox image={bgImg2} zIndex={1} />
-
-      {/* Fundo mais próximo */}
       <BackgroundBox image={bgImg1} zIndex={2} />
-      <Box>
-        <Box
-          sx={{
-            position: "absolute",
-            left: "50%",
-            transform: "translateX(-50%)",
-            zIndex: 1000,
-            textShadow: "1px 1px 0px #fff",
-            textAlign: "center",
-            color: "#cd9a02",
-            top: "16%"
-          }}
-        >
-          <Typography variant="h4">Olá! Você está no SAMS</Typography>
-        </Box>
-        <Card
-          sx={{
-            zIndex: 999,
-            boxShadow: "6px 6px 4px rgba(255, 193, 7, 1)", // Sombra amarela
-            position: "relative"
-          }}
-        >
-          <CardContent>
-            <Box display="flex" flexDirection="column" gap={2} width={250}>
-              <Typography variant="h6" align="center">
-                Identifique-se
-              </Typography>
-              <TextField
-                fullWidth
-                type="email"
-                label="Email"
-                value={email}
-                disabled={isLoading}
-                error={!!emailError}
-                helperText={emailError}
-                onChange={e => setEmail(e.target.value)}
-                onKeyDown={() => setEmailError("")}
-              />
-              <TextField
-                fullWidth
-                type={showPassword ? "text" : "password"}
-                label="Senha"
-                value={password}
-                disabled={isLoading}
-                error={!!passwordError}
-                helperText={passwordError}
-                onChange={e => setPassword(e.target.value)}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        onClick={() => setShowPassword(!showPassword)}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  )
-                }}
-                onKeyDown={() => setPasswordError("")}
-              />
-            </Box>
-          </CardContent>
-          <CardActions>
-            <Box width="100%" display="flex" justifyContent="center" gap={1}>
-              <Button
-                variant="contained"
-                onClick={handleSubmit}
-                disabled={isLoading}
-                endIcon={
-                  isLoading ? (
-                    <CircularProgress
-                      variant="indeterminate"
-                      color="inherit"
-                      size={20}
-                    />
-                  ) : undefined
-                }
-              >
-                Entrar
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => setRegister(true)}
-                disabled={isLoading}
-                endIcon={
-                  isLoading ? (
-                    <CircularProgress
-                      variant="indeterminate"
-                      color="inherit"
-                      size={20}
-                    />
-                  ) : undefined
-                }
-              >
-                Registrar
-              </Button>
-            </Box>
-          </CardActions>
-        </Card>
+
+      <Box
+        sx={{
+          position: "absolute",
+          top: "10%", // Ajuste para garantir que o título fique afastado do topo
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: 1000,
+          textShadow: "1px 1px 0px #fff",
+          textAlign: "center",
+          color: "#cd9a02",
+          width: "100%" // Garantir que o Box ocupe toda a largura
+        }}
+      >
+        <Typography variant="h4">Olá! Você está no SAMS</Typography>
       </Box>
+
+      <Card
+        sx={{
+          zIndex: 999,
+          boxShadow: "6px 6px 4px rgba(255, 193, 7, 1)", // Sombra amarela
+          position: "relative",
+          width: 300 // Tamanho fixo para evitar que o card se distorça em telas menores
+        }}
+      >
+        <CardContent>
+          <Box display="flex" flexDirection="column" gap={2} width={250}>
+            <Typography variant="h6" align="center">
+              Identifique-se
+            </Typography>
+            <TextField
+              fullWidth
+              type="email"
+              label="Email"
+              value={email}
+              disabled={isLoading}
+              error={!!emailError}
+              helperText={emailError}
+              onChange={e => setEmail(e.target.value)}
+              onKeyDown={() => setEmailError("")}
+            />
+            <TextField
+              fullWidth
+              type={showPassword ? "text" : "password"}
+              label="Senha"
+              value={password}
+              disabled={isLoading}
+              error={!!passwordError}
+              helperText={passwordError}
+              onChange={e => setPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+              onKeyDown={() => setPasswordError("")}
+            />
+          </Box>
+        </CardContent>
+        <CardActions>
+          <Box width="100%" display="flex" justifyContent="center" gap={1}>
+            <Button
+              variant="contained"
+              onClick={handleSubmit}
+              disabled={isLoading}
+              endIcon={
+                isLoading ? (
+                  <CircularProgress
+                    variant="indeterminate"
+                    color="inherit"
+                    size={20}
+                  />
+                ) : undefined
+              }
+            >
+              Entrar
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setRegister(true)}
+              disabled={isLoading}
+              endIcon={
+                isLoading ? (
+                  <CircularProgress
+                    variant="indeterminate"
+                    color="inherit"
+                    size={20}
+                  />
+                ) : undefined
+              }
+            >
+              Registrar
+            </Button>
+          </Box>
+        </CardActions>
+      </Card>
     </Box>
   );
 };
